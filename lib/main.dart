51bc3251610140998d86a3f84d86ab7176927930
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-06 09:19:57 -0400

    Colors and Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,26 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 82, 43, 234),
+        backgroundColor: Colors.purple[600],
         title: const Text('My First App'),
         centerTitle: true,
       ),
-      body: const Center(
-          child: Text('Hello, world')
+      body: Center(
+          child: Text(
+            'Hello, world',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'Handjet',
+            ),
+          )
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.purple[300],
         child: const Text('Click'),
-        onPressed: () {}
       ),
     ),
   ));
