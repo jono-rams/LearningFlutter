e845d64cb9945d4b67e49744e4a1cc405ac455ee
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-07 16:51:09 -0400

    Cards

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,40 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  //  List<String> quotes = [
-  //    'Be yourself; everyone else is already taken',
-  //    'I have nothing to declare except my genius',
-  //    'The truth is rarely pure and never simple'
-  //  ];
-
   List<Quote> quotes = [
     Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
     Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
     Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
   ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.red[600],
+              ),
+            ),
+            const SizedBox(height: 6.0,),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +62,7 @@ class _QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
